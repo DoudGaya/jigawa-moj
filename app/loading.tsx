efe684e283f9c1af9e764d85186c30afbb91468b
DoudGaya <arbdoule@yahoo.com>
2026-01-16 20:46:22 +0100

    cheet

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import logoicon from '@/public/img/icons/jigawa-icon.png'
+import spinner from '@/public/img/nigeria.png'
 
 const loading = () => {
   return (
     <div className=' h-screen w-full flex justify-center items-center'>
         <div className="">
-            <Image src={logoicon} className=' h-16 w-16 animate-pulse' alt='' />    
+            <Image src={spinner} className=' h-16 w-16 animate-pulse' alt='' />    
         </div>
     </div>
   )
